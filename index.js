e59fa9965bd0ca0a8d5fd65367aff86ed5be65db
kayla <kay.ann2000@gmail.com>
2022-03-15 18:08:39 -0600

    commit tree a3c599b114c6898e1138f0fd10276b1ca817ee5b

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution in this file!
+//const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo
+ const returnFirstTwoDrivers = function (drivers) {
+    let firstTwoDrivers = drivers.slice(0, 2)
+    return firstTwoDrivers 
+}
+const returnLastTwoDrivers = function (drivers) {
+    let lastTwoDrivers = drivers.slice(-2)
+    return lastTwoDrivers
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(number) {
+    return function(){
+        return number * number
+    }
+}
